dac21e007d0e1688f0d37a26798a37c13382b035
Tanyiqu <1953649096@qq.com>
2020-04-07 16:05:08 +0800

    项目基本框架

---
+++ UI/MainForm.py
@@ -0,0 +1,11 @@
+from UI.UI_MainForm import Ui_Form
+
+class MainForm(Ui_Form):
+    """
+    继承由pyuic生成的Form
+    在此文件里写信号槽
+    """
+
+    def init(self):
+
+        pass
---
+++ main/main.py
@@ -0,0 +1,12 @@
+import sys
+from PyQt5.Qt import QApplication,QWidget
+from UI.MainForm import MainForm
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ui = MainForm()
+    widget = QWidget()
+    ui.setupUi(widget)
+    # ui.init()
+    widget.show()
+    sys.exit(app.exec_())
